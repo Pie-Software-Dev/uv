44cb26c5e74cbf14c1abba28e893f10c10c07d93
HENRYMARTIN5 <62612165+HENRYMARTIN5@users.noreply.github.com>
2022-05-26 12:17:32 -0500

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: '/bare/',
+    bare: 'https://www.piesoftwaredev.repl.co/bare',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
